f45992a0d2d34a5d48ce65b2f190172d50186692
Pranav Jain <pranavji@tamu.edu>
2022-02-03 21:50:42 -0600

    added more features to site

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,8 +5,16 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J. K. Rowling'
+    fill_in 'Price', with: '50'
+    select '2017', :from => 'book[publish_date(1i)]'
+    select 'March', :from => 'book[publish_date(2i)]'
+    select '12', :from => 'book[publish_date(3i)]'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('J. K. Rowling')
+    expect(page).to have_content('50')
+    expect(page).to have_content('2017-03-12')
   end
 end
